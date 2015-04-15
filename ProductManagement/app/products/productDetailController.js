5e9faf33813503c6e1673ed21d17d733dadc2bfe
IgorKochetov <kochetovio@gmail.com>
2015-04-15 17:36:31 +0300

    Initial submit
    Large part of the application was developed locally without using
    GitHub, so 1st commit is a massive one, but more to come

---
+++ ProductManagement/app/products/productDetailController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+	"use strict";
+
+	angular
+		.module("productManagement")
+		.controller("ProductDetailController",
+		[
+			"product",
+			ProductDetailController
+		]);
+
+	function ProductDetailController (product) {
+		var vm = this;
+
+		vm.product = product;
+		vm.title = "Product detail: " + vm.product.productName;
+
+		if (vm.product.tags) {
+			vm.product.tagList = vm.product.tags.toString();
+		}
+	}
+}());
---
+++ ProductManagement/app/products/productListController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+	"use strict";
+	angular
+		.module("productManagement")
+		.controller("ProductListController", 
+			["productResource",
+				ProductListController]);
+
+	function ProductListController(productResource) {
+
+		var vm = this;
+
+		productResource.query(function(data) {
+			vm.products = data;
+		});
+		vm.showImage = false;
+
+		vm.toggleImage = function () {
+			vm.showImage = !vm.showImage;
+		};
+	}
+}());
